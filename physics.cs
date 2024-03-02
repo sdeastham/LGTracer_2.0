9c92a8c564816394d6bd9fd199a827b4b7a638ac
Sebastian D. Eastham <sdeastham@googlemail.com>
2024-03-02 21:58:35 +0000

    Implemented calculation of RH wrt ice

--- physics.cs
+++ physics.cs
@@ -20,6 +20,24 @@
                                      + 2.433502 * Math.Log(temperature)));
 
         }
+        
+        public static double SaturationPressureIce(double temperature)
+        {
+            /* DESCRIPTION:
+             * Returns water ice saturation pressure in Pascal.
+             * Source: Sonntag (1990) */
+
+            /* INPUT PARAMETERS:
+             * - double T :: temperature expressed in K
+             *
+             * OUTPUT PARAMETERS:
+             * - double :: H2O ice saturation pressure in Pascal */
+            return (100.0 * Math.Exp(-6024.5282 / temperature
+                                     + 24.7219 + 0.010613868 * temperature
+                                     - 1.3198825E-5 * temperature * temperature
+                                     - 0.49382577 * Math.Log(temperature)));
+
+        }
 
         private const double WaterMolarMass = 18.01528 * 1.0e-3; // kg/mol
         private const double AirMolarMass = 28.964 * 1.0e-3; // kg/mol
@@ -30,7 +48,13 @@
             // Returns relative humidity (fractional)
             // Temperature in K, pressure in Pa, specific humidity in kg water/kg air
             return (pressure * specificHumidity * WaterMolarConversion / SaturationPressureLiquid(temperature));
-
+        }
+        
+        public static double RelativeHumidityIce(double temperature, double pressure, double specificHumidity)
+        {
+            // Returns relative humidity (fractional)
+            // Temperature in K, pressure in Pa, specific humidity in kg water/kg air
+            return (pressure * specificHumidity * WaterMolarConversion / SaturationPressureIce(temperature));
         }
     }
 }
